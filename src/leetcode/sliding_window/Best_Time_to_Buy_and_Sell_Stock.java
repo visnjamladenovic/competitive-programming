5014eaf0b44b32136ffee0f064b63bfada90f21d
Visnja Mladenovic <visnjagithub@gmail.com>
2025-08-13 11:37:04 +0200

    Update : Best Time To Buy And Sell Stock

--- src/leetcode/sliding_window/Best_Time_to_Buy_and_Sell_Stock.java
+++ src/leetcode/sliding_window/Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,4 +1,20 @@
 package leetcode.sliding_window;
 
 public class Best_Time_to_Buy_and_Sell_Stock {
+    public int maxProfit(int[] prices) {
+        int left=0, right=1;
+        int maxProfit=0,profit=0;
+
+        while(right < prices.length){
+            if(prices[left] < prices[right]){
+                profit = prices[right] - prices[left];
+                maxProfit = Math.max(maxProfit,profit);
+            }
+            else{
+                left = right ;
+            }
+            right++;
+        }
+        return maxProfit;
+    }
 }
