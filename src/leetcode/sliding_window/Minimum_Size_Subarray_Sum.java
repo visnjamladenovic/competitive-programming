bfd9cc655780e0040bda3ee8906e8fb48a1ee33d
Visnja Mladenovic <visnjagithub@gmail.com>
2025-08-13 11:39:00 +0200

    Update: Minimum Size Subarray Sum

--- src/leetcode/sliding_window/Minimum_Size_Subarray_Sum.java
+++ src/leetcode/sliding_window/Minimum_Size_Subarray_Sum.java
@@ -1,4 +1,19 @@
 package leetcode.sliding_window;
 
 public class Minimum_Size_Subarray_Sum {
+    public int minSubArrayLen(int target, int[] nums) {
+        int result = Integer.MAX_VALUE;
+        int left=0,right=0,sum=0;
+
+        for(right = 0; right<nums.length;right++){
+            sum+=nums[right];
+            while(sum >= target){
+                result = Math.min(right - left + 1, result);
+                sum = sum - nums[left];
+                left++;
+            }
+        }
+        return result == Integer.MAX_VALUE ? 0 : result;
+
+    }
 }
