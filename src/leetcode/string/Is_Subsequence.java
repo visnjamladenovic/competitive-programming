21432331cd52c611b2579070f08bebc3c8b5b786
Visnja Mladenovic <visnjagithub@gmail.com>
2025-08-13 16:12:29 +0200

    Solve LeetCode 392: Is Subsequence

---
+++ src/leetcode/string/Is_Subsequence.java
@@ -0,0 +1,18 @@
+package leetcode.string;
+
+public class Is_Subsequence {
+    public boolean isSubsequence(String s, String t) {
+
+        int i = 0;
+        int j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+}
