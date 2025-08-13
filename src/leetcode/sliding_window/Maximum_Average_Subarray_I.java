18023477f3700ff9296b9598afe4de9b3e163037
Visnja Mladenovic <visnjagithub@gmail.com>
2025-08-13 11:37:57 +0200

    Update: Maximum Size Subarray Sum

--- src/leetcode/sliding_window/Maximum_Average_Subarray_I.java
+++ src/leetcode/sliding_window/Maximum_Average_Subarray_I.java
@@ -1,4 +1,22 @@
 package leetcode.sliding_window;
 
 public class Maximum_Average_Subarray_I {
+        public double findMaxAverage(int[] nums, int k) {
+            double sum=0,maxSum=0;
+            int i=0;
+            if(nums.length ==1){
+                return nums[i];
+            }
+
+            for( i=0; i<k; i++){
+                sum+=nums[i];
+            }
+
+            maxSum=sum;
+            for( i=k; i<nums.length; i++){
+                sum += nums[i] - nums[i - k];
+                maxSum = Math.max(maxSum, sum);
+            }
+            return maxSum/k;
+        }
 }
