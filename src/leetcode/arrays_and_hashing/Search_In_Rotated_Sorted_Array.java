1db56fc54887062bd8882da58d25ae2dc80194cc
Visnja Mladenovic <visnjagithub@gmail.com>
2025-08-19 00:04:12 +0200

    Solve LeetCode 33: Search In Rotated Sorted Array

---
+++ src/leetcode/arrays_and_hashing/Search_In_Rotated_Sorted_Array.java
@@ -0,0 +1,33 @@
+package leetcode.arrays_and_hashing;
+
+public class Search_In_Rotated_Sorted_Array {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
