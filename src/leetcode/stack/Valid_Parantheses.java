9bc8638948ae8e1da9347ffd9bd98020ac3d8f3b
Visnja Mladenovic <visnjagithub@gmail.com>
2025-08-19 00:22:27 +0200

    Solve LeetCode 20: Valid Parentheses

---
+++ src/leetcode/stack/Valid_Parantheses.java
@@ -0,0 +1,27 @@
+package leetcode.stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+
+public class Valid_Parantheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')','(');
+        map.put(']','[');
+        map.put('}','{');
+
+        for(char c: s.toCharArray()){
+            if(map.containsValue(c)){
+                stack.push(c);
+            } else if(map.containsKey(c)){
+                if(stack.isEmpty() || map.get(c) != stack.pop()){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
