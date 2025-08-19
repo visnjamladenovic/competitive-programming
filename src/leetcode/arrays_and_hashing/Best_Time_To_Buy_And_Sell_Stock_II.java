ac70320c71681ecbcd453f4e2c659d12953a31c0
Visnja Mladenovic <visnjagithub@gmail.com>
2025-08-20 00:32:33 +0200

    Solve LeetCode 122: Best Time to Buy and Sell Stock II

---
+++ src/leetcode/arrays_and_hashing/Best_Time_To_Buy_And_Sell_Stock_II.java
@@ -0,0 +1,13 @@
+package leetcode.arrays_and_hashing;
+
+public class Best_Time_To_Buy_And_Sell_Stock_II {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
